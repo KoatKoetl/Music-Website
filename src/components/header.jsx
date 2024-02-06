123b08940f11650aceb816603b001080ed3e1593
KoatKoetl <stas20027@outlook.com>
2024-02-07 01:37:29 +0200

    Changes: website header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,17 @@
 const Header = () => {
   return (
-    <header className="sticky top-0 z-50 mb-2 h-12 w-full bg-black bg-opacity-50"></header>
+    <header className="sticky top-0 z-50 mb-2 grid h-12 w-full bg-black bg-opacity-50 2xl:justify-center">
+      <div className="mx-5 flex h-full  items-center justify-between 2xl:w-[1500px]">
+        <img src="" alt="" width={40} height={40} />
+        <nav>
+          <ul className="flex items-center">
+            <li>
+              <a href="">Home</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 };
 
