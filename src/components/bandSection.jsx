0ae7db6213a97837ae07c3aa30b1b459fafce9f5
KoatKoetl <stas20027@outlook.com>
2024-02-07 04:25:59 +0200

    Work: section band

--- src/components/bandSection.jsx
+++ src/components/bandSection.jsx
@@ -1,7 +1,56 @@
+import bandPhoto from "../assets/images/band-KINO.webp";
+
 const BandSection = () => {
   return (
-    <section className="bg-dark-pink h-svh">
-      <div className="my-10 flex justify-center py-10"></div>
+    <section className="bg-dark-pink font-Rubik h-svh">
+      <div className="bg-dark-pink flex px-16 py-10">
+        <div className="shadow-poster max-w-[650px] bg-dark-gray p-4">
+          <h3 className="text-center text-[13rem] font-bold leading-[180px] tracking-tighter">
+            К<span className="font-normal">И</span>НО
+          </h3>
+          <div className="grid">
+            <img
+              src={bandPhoto}
+              alt=""
+              width={600}
+              className="justify-self-center rounded-lg"
+            />
+          </div>
+          <div className="grid grid-cols-2">
+            <ul className="p-4">
+              <h4 className="text-center text-xl font-semibold">
+                Band Members
+              </h4>
+              <li>
+                <span className="font-semibold">Lead Vocalist:</span> Viktor
+                Robertovich Tsoi -{" "}
+                <a
+                  href="https://en.wikipedia.org/wiki/Viktor_Tsoi"
+                  target="blank"
+                >
+                  Wikipedia
+                </a>
+              </li>
+              <li>
+                <span className="font-semibold">Lead Vocalist:</span> Viktor
+                Robertovich Tsoi - <a href="">Wikipedia</a>
+              </li>
+              <li>
+                <span className="font-semibold">Lead Vocalist:</span> Viktor
+                Robertovich Tsoi - <a href="">Wikipedia</a>
+              </li>
+              <li>
+                <span className="font-semibold">Lead Vocalist:</span> Viktor
+                Robertovich Tsoi - <a href="">Wikipedia</a>
+              </li>
+            </ul>
+            <ul className="p-4">
+              <h4 className="text-center text-xl font-semibold">Albums</h4>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
