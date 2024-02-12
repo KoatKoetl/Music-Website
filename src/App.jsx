57e8d0371101ac00a5f58ab696fce35b83900781
KoatKoetl <stas20027@outlook.com>
2024-02-12 15:49:49 +0200

    Change: import adress

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import BandSection from "./components/BandSection/bandSection";
-import Header from "./components/header";
-import Main from "./components/main";
+import BandSection from "/src/components/BandSection/bandSection";
+import Header from "/src/components/header";
+import Main from "/src/components/main";
 
 const App = () => {
   return (
