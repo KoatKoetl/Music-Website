b56e9487c6d55e8806a5a2cf63130688210969cb
KoatKoetl <stas20027@outlook.com>
2024-02-12 15:54:27 +0200

    Create: new component PlayList

--- src/components/BandSection/bandSection.jsx
+++ src/components/BandSection/bandSection.jsx
@@ -1,9 +1,11 @@
 import Poster from "./Poster/poster";
+import PlayList from "./playList";
 
 const BandSection = () => {
   return (
     <section className="font-Rubik">
       <Poster />
+      <PlayList />
     </section>
   );
 };
