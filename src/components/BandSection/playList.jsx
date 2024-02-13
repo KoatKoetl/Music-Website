b649d3530b0cdb1a2fb129ae08499b196281103d
KoatKoetl <stas20027@outlook.com>
2024-02-13 23:00:47 +0200

    Changes: 1. Styles for responsivness 2. Add suspense on music image covers + title for playlist 3. Add link to full song on Deezer

--- src/components/BandSection/playList.jsx
+++ src/components/BandSection/playList.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import API_Call from "../../utils/API/Deezer_API";
+import LazyLoadedImage from "../LazyLoad/lazyLoadImage";
 
 // const mockAPI_DATA = [
 //   {
@@ -109,7 +110,7 @@ const PlayList = () => {
 
   if (loading) {
     return (
-      <div className="flex w-[850px] items-center justify-center p-6">
+      <div className="flex flex-1 items-center justify-center p-6">
         Loading...
       </div>
     );
@@ -117,37 +118,50 @@ const PlayList = () => {
 
   if (!playList && playList.length === 0) {
     return (
-      <div className="flex w-[850px] items-center justify-center p-6">
+      <div className="flex flex-1 items-center justify-center p-6">
         No playlist data found.
       </div>
     );
   }
 
   return (
-    <div className="min-w-[300px] flex-1 p-6">
-      <ul className="grid max-h-[800px] gap-4 overflow-scroll">
+    <div className="min-w-[600px] max-w-[1150px] flex-1">
+      <h3 className="text-center text-4xl font-semibold">
+        Short playlist example
+      </h3>
+      <ul className="grid max-h-[770px] overflow-scroll">
         {playList.map((track, index) => (
           <li
             key={track.id}
-            className="flex gap-2 rounded-sm p-2 hover:bg-dark-gray hover:shadow-xl"
+            className="flex flex-wrap gap-2 rounded-sm p-2 hover:bg-dark-gray hover:shadow-xl"
           >
-            <div className="relative">
+            <div className="screen-1400:size-24 relative size-20">
               <span className="absolute left-1 z-10 font-bold">
                 {index + 1}
               </span>
-              <img
-                src={track.album.cover}
-                alt={"Song" + track.title + "cover image"}
-                className="relative z-0 size-24"
-              />
+              <Suspense
+                fallback={
+                  <div className="relative z-0 flex h-full w-full items-center justify-center">
+                    Loading...
+                  </div>
+                }
+              >
+                <LazyLoadedImage
+                  src={track.album.cover}
+                  alt={"Song " + track.title + " cover image"}
+                  styles={"relative z-0 h-full w-full text-sm text-center"}
+                />
+              </Suspense>
             </div>
             <div className="flex flex-col justify-center">
-              <h5 className="text-2xl font-semibold">{track.title}</h5>
-              <h6 className="text-md opacity-80 hover:opacity-100">
+              <h5 className="screen-1400:text-2xl text-lg font-semibold">
+                {track.title}
+              </h5>
+              <h6 className="screen-1400:text-md text-sm opacity-80 hover:opacity-100">
                 <span className="font-semibold">Artist name:</span>{" "}
                 {track.artist.name}
               </h6>
-              <h6 className="text-md opacity-80 hover:opacity-100">
+              <h6 className="screen-1400:text-md text-sm opacity-80 hover:opacity-100">
                 <span className="font-semibold">Album:</span>{" "}
                 {track.album.title}
               </h6>
@@ -157,6 +171,18 @@ const PlayList = () => {
               <audio controls>
                 <source src={track.preview} type="audio/ogg" />
               </audio>
+            </div>
+            <div className="flex min-w-[200px] flex-1 items-center justify-center">
+              <a
+                href={track.link}
+                className="whitespace-break-spaces text-center hover:drop-shadow-font-shadow-2"
+              >
+                <h5>
+                  <strong>Deezer</strong>
+                  <br />
+                  Click here and listen full song
+                </h5>
+              </a>
             </div>
           </li>
         ))}
