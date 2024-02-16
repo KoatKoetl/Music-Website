68d3fabfcb743f35260e212eee2b8037ff160199
KoatKoetl <stas20027@outlook.com>
2024-02-17 00:58:45 +0200

    Activate sourcemap

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   plugins: [react()],
   test: {
     globals: true,
