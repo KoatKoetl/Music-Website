71561e83aff568f9a5d3296a3c1028eec4c7a5d7
KoatKoetl <stas20027@outlook.com>
2024-02-19 01:14:43 +0200

    Style changes

--- src/components/BandSection/bandSection.jsx
+++ src/components/BandSection/bandSection.jsx
@@ -4,7 +4,7 @@ import Poster from "./Poster/poster";
 const BandSection = () => {
   return (
     <section className="font-Rubik">
-      <div className="flex flex-wrap items-center justify-center gap-4 bg-dark-pink p-5 xl:items-start xl:justify-normal 2xl:justify-center 2xl:py-10">
+      <div className="flex flex-wrap items-center justify-center gap-12 bg-dark-pink p-5 xl:items-start xl:justify-normal 2xl:justify-center 2xl:py-10">
         <Poster />
         <PlayList />
       </div>
