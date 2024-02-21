9063cdf5925522331667273e54a9dc04b6447fc1
KoatKoetl <stas20027@outlook.com>
2024-02-22 01:19:12 +0200

    Fix: flex aligning

--- src/components/BandSection/bandSection.jsx
+++ src/components/BandSection/bandSection.jsx
@@ -4,7 +4,7 @@ import Poster from "./Poster/poster";
 const BandSection = () => {
   return (
     <section className="font-Rubik">
-      <div className="flex flex-wrap gap-12 bg-dark-pink p-3 lil:p-5 2xl:py-10">
+      <div className="flex flex-wrap justify-center gap-12 bg-dark-pink p-3 lil:p-5 2xl:py-10">
         <Poster />
         <PlayList />
       </div>
