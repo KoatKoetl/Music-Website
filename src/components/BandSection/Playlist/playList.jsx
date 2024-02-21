12c16f69ac0dc47f0bca912fa94214282862330b
KoatKoetl <stas20027@outlook.com>
2024-02-22 01:19:22 +0200

    Change: margins

--- src/components/BandSection/Playlist/playList.jsx
+++ src/components/BandSection/Playlist/playList.jsx
@@ -4,7 +4,7 @@ import SongList from "./SongList";
 const PlayList = () => {
   return (
     <div className="min-w-[250px] max-w-[1200px] flex-1 sm:min-w-[600px]">
-      <h3 className="mb-2 text-center text-3xl font-semibold sm:mb-6 sm:text-4xl">
+      <h3 className="mb-2 text-center text-3xl font-semibold sm:text-4xl">
         Playlist from Deezer
       </h3>
       <SongList />
