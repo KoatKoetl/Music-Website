1e0a069e7f7744c09ccc0ef3211aacede1e65cbc
KoatKoetl <stas20027@outlook.com>
2024-02-23 00:28:07 +0200

    Fix: remove spin animation because of lags

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -39,7 +39,7 @@ const Main = () => {
 
   return (
     <main className="mb-20 flex min-h-[90svh] items-center justify-center">
-      <div className="mediaPointer:hover:shadow-10xl mediaTouch:active:shadow-10xl relative flex size-[260px] flex-col items-center justify-center overflow-hidden rounded-full bg-transparent p-10 text-center font-DMSerifDisplay transition-all duration-500 ease-in-out lil:size-[400px] sm:size-[500px] md:size-[650px] lg:size-[600px] xl:size-[700px] 2xl:size-[800px] 4xl:size-[1000px]">
+      <div className="relative flex size-[260px] flex-col items-center justify-center overflow-hidden rounded-full bg-transparent p-10 text-center font-DMSerifDisplay transition-all duration-500 ease-in-out lil:size-[400px] sm:size-[500px] md:size-[650px] lg:size-[600px] xl:size-[700px] 2xl:size-[800px] 4xl:size-[1000px] mediaPointer:hover:shadow-10xl mediaTouch:active:shadow-10xl">
         <h1 className="title leading-0 relative z-10 flex lg:mb-4">
           <span className="text-wrapper 4x:text-12xl relative inline-block overflow-hidden text-2xl font-bold lil:text-4xl lil:leading-normal sm:text-5xl md:text-7xl lg:text-6xl xl:text-7xl 2xl:text-8xl">
             <span className="letters">Hello everyone,</span>
@@ -56,7 +56,7 @@ const Main = () => {
         <img
           src={guitarImageLogo}
           alt="photo of guitar in black and white from top of freatboard"
-          className="background-image absolute left-0 top-0 h-full w-full animate-spin-slow rounded-full bg-cover bg-center opacity-0"
+          className="background-image absolute left-0 top-0 h-full w-full rounded-full bg-cover bg-center opacity-0"
         />
       </div>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -114,7 +114,6 @@ export default {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "spin-slow": "spin 30s linear infinite",
       },
     },
   },
