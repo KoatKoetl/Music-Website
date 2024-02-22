a0b5277a540aa4b8140f3f2ea077d2e7e013a447
KoatKoetl <stas20027@outlook.com>
2024-02-22 02:24:58 +0200

    Change the title

--- src/components/BandSection/Playlist/playList.jsx
+++ src/components/BandSection/Playlist/playList.jsx
@@ -5,7 +5,7 @@ const PlayList = () => {
   return (
     <div className="min-w-[250px] max-w-[1200px] flex-1 sm:min-w-[600px]">
       <h3 className="mb-2 text-center text-3xl font-semibold sm:text-4xl">
-        Playlist from Deezer
+        Playlist
       </h3>
       <SongList />
     </div>
