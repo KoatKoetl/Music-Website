3ee8c2b3b8bd58902c569014dae90ba6cb9726ba
KoatKoetl <stas20027@outlook.com>
2024-02-25 00:27:00 +0200

    Fix: overflow problem

--- src/components/KINO-Section/Poster/poster.jsx
+++ src/components/KINO-Section/Poster/poster.jsx
@@ -5,7 +5,7 @@ import BandMembersPhoto from "./bandMembersPhoto";
 const Poster = () => {
   return (
     <section
-      className="flex min-w-[250px] max-w-[650px] flex-1 rotate-[20deg] -skew-x-6 -skew-y-12 scale-50 transform-gpu flex-col items-center justify-center bg-dark-gray p-2 shadow-poster transition-all duration-1000 hover:rotate-[360deg] hover:skew-x-0 hover:skew-y-0 hover:scale-100 lil:min-w-[435px] sm:p-4 md:min-w-[650px]"
+      className="flex min-w-[250px] max-w-[650px] flex-1 rotate-[20deg] scale-50 transform-gpu flex-col items-center justify-center overflow-hidden bg-dark-gray p-2 shadow-poster transition-all duration-1000 hover:rotate-[360deg] hover:skew-x-0 hover:skew-y-0 hover:scale-100 lil:min-w-[435px] lil:-skew-x-6 lil:-skew-y-12 sm:p-4 md:min-w-[650px]"
       aria-label="custom made KINO band poster"
     >
       <h3 className="text-center font-bold leading-[1] tracking-tighter [font-size:_clamp(2em,30vw,13em)] md:text-[13rem] md:leading-[180px]">
