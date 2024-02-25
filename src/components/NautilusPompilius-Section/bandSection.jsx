a79c76350b1d87e3a60d5559a57a378cff430e23
KoatKoetl <stas20027@outlook.com>
2024-02-26 01:01:34 +0200

    Fix: padding bottom to close to next section

--- src/components/NautilusPompilius-Section/bandSection.jsx
+++ src/components/NautilusPompilius-Section/bandSection.jsx
@@ -4,7 +4,7 @@ import Poster from "./Poster/poster";
 const BandSection = () => {
   return (
     <section className="font-PlayfarDisplay" id="section-Nautilus">
-      <div className="flex flex-wrap justify-center gap-12 bg-black p-3 lil:p-5 2xl:py-10">
+      <div className="flex flex-wrap justify-center gap-12 bg-black p-3 pb-8 lil:p-5 2xl:py-10">
         <Poster />
         <PlayList />
       </div>
