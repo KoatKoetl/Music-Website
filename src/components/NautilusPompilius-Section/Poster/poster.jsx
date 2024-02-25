c505c84b1d203c2d306d818c23d87e6ccfbbdcd6
KoatKoetl <stas20027@outlook.com>
2024-02-26 01:42:44 +0200

    Sort tailwind classes

--- src/components/NautilusPompilius-Section/Poster/poster.jsx
+++ src/components/NautilusPompilius-Section/Poster/poster.jsx
@@ -5,7 +5,7 @@ import BandMembersPhoto from "./bandMembersPhoto";
 const Poster = () => {
   return (
     <section
-      className="shadow-inversePoster lil:skew-y-4 flex min-w-[250px] max-w-[650px] flex-1 -rotate-[20deg] scale-50 transform-gpu flex-col items-center justify-center overflow-hidden bg-gradient-to-t from-[#1e3b7288] via-[#1e3b72b2] to-[#2a529894] p-2 shadow-gray-700 transition-all duration-1000 hover:rotate-[360deg] hover:skew-x-0 hover:skew-y-0 hover:scale-100 lil:min-w-[435px] lil:skew-x-12 sm:p-4 md:min-w-[650px]"
+      className="lil:skew-y-4 flex min-w-[250px] max-w-[650px] flex-1 -rotate-[20deg] scale-50 transform-gpu flex-col items-center justify-center overflow-hidden bg-gradient-to-t from-[#1e3b7288] via-[#1e3b72b2] to-[#2a529894] p-2 shadow-inversePoster shadow-gray-700 transition-all duration-1000 hover:rotate-[360deg] hover:skew-x-0 hover:skew-y-0 hover:scale-100 lil:min-w-[435px] lil:skew-x-12 sm:p-4 md:min-w-[650px]"
       aria-label="custom made Nautilus Pompilius band poster"
     >
       <h3 className="text-center font-bold leading-[1] tracking-tighter [font-size:_clamp(2em,16svw,7em)] md:text-[7rem]">
