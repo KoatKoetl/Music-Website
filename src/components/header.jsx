b789f6dc34a84a293eaf787f5f6ec8f706274261
KoatKoetl <stas20027@outlook.com>
2024-02-26 01:42:02 +0200

    Fix: responsivness Change: add burger menu, reworked logic for DropdownItems render

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,62 +3,23 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuPortal,
   DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
-
-const RenderDropDownMenu = () => {
-  const [sectionIds, setSectionIds] = useState([]);
-
-  useEffect(() => {
-    const generateSectionIds = () => {
-      const sectionIdTemplate = "section-";
-      const ids = [];
-
-      document.querySelectorAll("section").forEach((element) => {
-        if (element.id && element.id.startsWith(sectionIdTemplate)) {
-          ids.push(element.id);
-        }
-      });
-
-      return ids;
-    };
-
-    setSectionIds(generateSectionIds());
-  }, []);
-
-  return (
-    <>
-      <DropdownMenu>
-        <DropdownMenuTrigger className="rounded-md px-2 text-sm sm:text-base mediaPointer:hover:bg-accent mediaPointer:hover:text-accent-foreground mediaTouch:active:bg-accent mediaTouch:active:text-accent-foreground">
-          Artists
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="mt-2.5 border-2 bg-dark-gray text-white">
-          <DropdownMenuLabel>
-            <span className="text-sm sm:text-base">Artist sections</span>
-          </DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          {sectionIds.map((id) => (
-            <DropdownMenuItem key={id} className="p-0">
-              <a
-                href={`#${id}`}
-                className="flex-1 px-2 py-1.5 text-sm sm:text-base"
-              >
-                {id.split("-").pop()}
-              </a>
-            </DropdownMenuItem>
-          ))}
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </>
-  );
-};
 
 const NavBar = () => {
   return (
     <nav className="my-2 flex">
-      <ul className="flex gap-2 sm:gap-10">
+      <BurgerMenu />
+
+      <ul className="hidden gap-2 lil:flex sm:gap-10">
         <li className="rounded-md px-2 mediaPointer:hover:bg-accent mediaPointer:hover:text-accent-foreground mediaTouch:active:bg-accent mediaTouch:active:text-accent-foreground">
           <a
             href="#main"
@@ -67,7 +28,20 @@ const NavBar = () => {
             Home
           </a>
         </li>
-        <RenderDropDownMenu />
+        <li className="flex">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="rounded-md px-2 text-sm sm:text-base mediaPointer:hover:bg-accent mediaPointer:hover:text-accent-foreground mediaTouch:active:bg-accent mediaTouch:active:text-accent-foreground">
+              Artists
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="mt-2.5 border-2 bg-dark-gray text-white">
+              <DropdownMenuLabel>
+                <span className="text-sm sm:text-base">Artist sections</span>
+                <DropdownMenuSeparator />
+                {RenderSections()}
+              </DropdownMenuLabel>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </li>
         <li className="rounded-md px-2 mediaPointer:hover:bg-accent mediaPointer:hover:text-accent-foreground mediaTouch:active:bg-accent mediaTouch:active:text-accent-foreground">
           <a
             href="#tools"
@@ -97,10 +71,85 @@ const LogoImage = () => {
   );
 };
 
+const BurgerMenu = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex items-center justify-center rounded-md px-2 text-sm lil:hidden sm:text-base mediaPointer:hover:bg-accent mediaPointer:hover:text-accent-foreground mediaTouch:active:bg-accent mediaTouch:active:text-accent-foreground">
+        <FontAwesomeIcon icon={faBars} />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="mt-2.5 border-2 bg-dark-gray text-white">
+        <DropdownMenuItem>
+          {" "}
+          <a
+            href="#main"
+            className="flex h-full items-center text-sm sm:text-base "
+          >
+            Home
+          </a>
+        </DropdownMenuItem>
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger className="border-2 bg-dark-gray text-white mediaPointer:hover:bg-accent mediaPointer:hover:text-accent-foreground mediaTouch:focus:bg-accent mediaTouch:focus:text-accent-foreground mediaTouch:active:bg-accent mediaTouch:active:text-accent-foreground ">
+            Artists
+          </DropdownMenuSubTrigger>
+          <DropdownMenuPortal>
+            <DropdownMenuSubContent className="border-2 bg-dark-gray text-white">
+              {RenderSections()}
+            </DropdownMenuSubContent>
+          </DropdownMenuPortal>
+        </DropdownMenuSub>
+        <DropdownMenuItem>
+          <a
+            href="#tools"
+            className="flex h-full items-center text-sm sm:text-base "
+          >
+            Used Tools
+          </a>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+const RenderSections = () => {
+  const [sectionIds, setSectionIds] = useState([]);
+
+  useEffect(() => {
+    const generateSectionIds = () => {
+      const sectionIdTemplate = "section-";
+      const ids = [];
+
+      document.querySelectorAll("section").forEach((element) => {
+        if (element.id && element.id.startsWith(sectionIdTemplate)) {
+          ids.push(element.id);
+        }
+      });
+
+      return ids;
+    };
+
+    setSectionIds(generateSectionIds());
+  }, []);
+
+  return (
+    <>
+      {sectionIds.map((id) => (
+        <DropdownMenuItem key={id} className="p-0">
+          <a
+            href={`#${id}`}
+            className="flex-1 px-2 py-1.5 text-sm sm:text-base"
+          >
+            {id.split("-").pop()}
+          </a>
+        </DropdownMenuItem>
+      ))}
+    </>
+  );
+};
+
 const Header = () => {
   return (
     <header className="sticky top-0 z-50 mb-2 grid h-12 w-full bg-dark-gray shadow-md 2xl:justify-center">
-      <div className="mx-1 flex justify-between sm:mx-5 2xl:w-[1500px]">
+      <div className="mx-5 flex justify-between sm:mx-6 2xl:w-[1500px]">
         <LogoImage />
         <NavBar />
       </div>
