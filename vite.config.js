ae313076c5abc9ea6bed46268d87db0f03ed0992
KoatKoetl <stas20027@outlook.com>
2024-02-26 02:42:18 +0200

    Change: try to fix 404 gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   build: {
     sourcemap: true,
   },
@@ -16,7 +17,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@assets": path.resolve(__dirname, "src/assets"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
 });
