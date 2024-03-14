c27881b348486b4f3721f86b2a4799faa8a2c2bf
KoatKoetl <stas20027@outlook.com>
2024-03-14 23:38:30 +0200

    Revert "Clear" Fix
    This reverts commit 9c3dce90fce328f3194c945680c38c8713e328c7.

--- src/App.test.jsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import App from './App';
-
-describe('App component', () => {
-  it('renders magnificent monkeys', () => {});
-});
