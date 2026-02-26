9526c320c93ad33b13df270f8c98b0f3223e9f7e
Stanislav Suschevici <stas20027@outlook.com>
2026-02-26 17:09:32 +0200

    (FIX) - BandMembersPhoto not rendering image correctly

--- src/components/Section/Poster/BandMembersPhoto.jsx
+++ src/components/Section/Poster/BandMembersPhoto.jsx
@@ -4,18 +4,15 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 const BandMembersPhoto = ({ membersPhoto }) => {
   const { src, alt, styles, effect, placeholderSrc } = membersPhoto;
 
-  // Band Members image props
-  const bandPhotoProps = {
-    src: src,
-    alt: alt,
-    styles: styles,
-    effect: effect,
-    placeholderSrc: placeholderSrc,
-  };
-
   return (
     <div className="relative flex min-w-[250px] max-w-[600px] flex-1 items-center justify-center lil:min-w-[400px]">
-      <LazyLoadImage {...bandPhotoProps} className="h-full w-full" />
+      <LazyLoadImage
+        src={src}
+        alt={alt}
+        className={`h-full w-full ${styles || ""}`}
+        effect={effect || "blur"}
+        placeholderSrc={placeholderSrc}
+      />
     </div>
   );
 };
