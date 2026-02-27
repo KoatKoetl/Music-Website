b95d4a9c7c3e75f825604c639e0bb11cba89c75a
Stanislav Suschevici <stas20027@outlook.com>
2026-02-27 14:52:35 +0200

    fix: configure vitest to use built-in v8 coverage

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,16 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/tests/setup.js",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      exclude: [
+        "node_modules/",
+        "src/tests/",
+        "**/*.config.js",
+        "**/*.config.cjs",
+      ],
+    },
   },
   resolve: {
     alias: {
